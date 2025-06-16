14995a3c1c803b3a4459cb2c654bdf1545c9765b
amplifyxlabs <amplifyxlabstech@gmail.com>
2025-06-16 18:10:14 +0530

    rm

--- src/app/cancellation-policy/page.tsx
+++ src/app/cancellation-policy/page.tsx
@@ -19,7 +19,6 @@ export default function CancellationPolicy() {
             <li>If refund is approved, the amount will be credited within 7-14 days to the customer bank account.</li>
             <li>Cancellations will be considered only if the request is made immediately after placing the order.</li>
             <li>However, the cancellation request may not be entertained if the orders have been communicated to the vendors/merchants and they have initiated the process of shipping them.</li>
-            <li>Kadala does not accept cancellation requests for perishable items like flowers, eatables etc.</li>
             <li>However, refund/replacement can be made if the customer establishes that the quality of product delivered is not good.</li>
             <li>In case of receipt of damaged or defective items please report the same to our Customer Service team.</li>
             <li>The request will, however, be entertained once the merchant has checked and determined the same at his own end.</li>
